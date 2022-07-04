b98f6b64ef03dd9c9daa2f37119c7b0ceff6f49a
Neil-Vilarroel <neil.villarroel@hotmail.com>
2022-07-04 12:00:02 -0400

    erick
    Creacion de super usuario y modelos

--- sistema/bodega/admin.py
+++ sistema/bodega/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Articulo
+from .models import Categoria
 
 # Register your models here.
+admin.site.register(Articulo)
+admin.site.register(Categoria)
+
--- sistema/bodega/models.py
+++ sistema/bodega/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# Modelo Categoria
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name ='Id Categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name = 'Nombre Categoria')
+
+    def __str__(self):
+        return self.nombreCategoria
+
+# Modelo Articulos
+
+class Articulo(models.Model):
+    Nombre = models.CharField(max_length=50, verbose_name = 'Nombre')
+    Modelo = models.CharField(max_length=50, null=True, blank=True, verbose_name = 'Modelo')
+    Ubicacion = models.CharField(max_length=50,null=True,blank=True,verbose_name ='Ubicacion')
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Nombre
+
